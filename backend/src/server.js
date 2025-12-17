7e123739e4b27df63823830d65d99eee0bf1897f
Jorge carvajal <carvajalyork89@gmail.com>
2025-12-17 13:36:12 +0100

    chore: log node version at startup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@
 import "./load-env.js";
 import app from "./app.js";
 
+console.log("🟢 Node version:", process.version);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
